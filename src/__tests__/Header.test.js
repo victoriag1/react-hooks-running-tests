2e866dd2dcd7f2c00b2ad909166a6b9d2934aea2
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-03-30 14:11:08 -0700

    commit tree fcf4354de295697a44fb55d01b3e6595ec6a895c

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
